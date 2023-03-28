3013c7bbf44cef97680aa233b1b198b18da612aa
rishpotti <112750870+rishpotti@users.noreply.github.com>
2023-03-28 12:17:57 +0530

    Add files via upload

---
+++ Fashion.py
@@ -0,0 +1,69 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),  # input
+    keras.layers.Dense(128, activation='relu'),  # hidden
+    keras.layers.Dense(10, activation='softmax') # output
+])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
+
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=1) 
+print('Test accuracy:', test_acc)
+
+COLOR = 'white'
+plt.rcParams['text.color'] = COLOR
+plt.rcParams['axes.labelcolor'] = COLOR
+
+def predict(model, image, correct_label):
+  class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+  prediction = model.predict(np.array([image]))
+  predicted_class = class_names[np.argmax(prediction)]
+  print("Prediction: " + predicted_class)
+  print("Actual: " + class_names[correct_label])  
+  show_image(image, class_names[correct_label], predicted_class)
+
+
+def show_image(img, label, guess):
+  plt.figure()
+  plt.imshow(img, cmap=plt.cm.binary)
+  plt.title("Excpected: " + label)
+  plt.xlabel("Guess: " + guess)
+  plt.colorbar()
+  plt.grid(False)
+  plt.show()
+
+
+def get_number():
+  while True:
+    num = input("Pick a number: ")
+    if num.isdigit():
+      num = int(num)
+      if 0 <= num <= 1000:
+        return int(num)
+    else:
+      print("Try again...")
+
+num = get_number()
+image = test_images[num]
+label = test_labels[num]
+predict(model, image, label)
+
